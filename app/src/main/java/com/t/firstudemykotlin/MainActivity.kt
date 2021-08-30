ec2da7fc8467a88b5de1beb0502bef74245509ce
Timofey <tsarionov.timofey@letmecode.org>
2021-08-30 17:43:40 +0300

    complete

--- app/src/main/java/com/t/firstudemykotlin/MainActivity.kt
+++ app/src/main/java/com/t/firstudemykotlin/MainActivity.kt
@@ -2,10 +2,15 @@ package com.t.firstudemykotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.t.firstudemykotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.buttonShow.setOnClickListener { binding.textViewName.text = "Hello, ${binding.editTextName.text.trim()}" }
     }
 }
